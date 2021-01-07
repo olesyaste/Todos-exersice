92c2e25d1e79d9825ffff870f6ad2a213e4dc061
Olesya Steinmetz <newsabome@icloud.com>
2021-01-07 12:21:05 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+const axios = require('axios');
+
+const list = ['test1', 'test2', 'test3'];
+
+class App extends React.Component {
+  state = {
+    todos: []
+  }
+
+componentDidMount(){
+axios.get(`https://jsonplaceholder.typicode.com/todos/`)
+    .then(res => {
+      const todos = res.data;
+      console.log(todos)
+      this.setState({ todos });
+  })
+}
+
+render() {
+
+  return (
+    <>
+      <ul>
+        {list.map(el => {
+         return <li style={{color: 'green'}}>{el}</li>
+        })}
+      </ul>
+      <ol>
+        { this.state.todos.slice(0,10).map(todo =>
+        <li style={{color: 'red'}} key={todo.id}>{todo.title}</li>) }
+      </ol>
+   </>
+  );
+ }
+}
+export default App;
